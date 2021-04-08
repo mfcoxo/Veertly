f9f91842dd5307db245552629910091478117086
Brittany Blake <brittany.blake49@gmail.com>
2021-04-08 02:40:14 +0200

    add redux and files

---
+++ src/ParticipantList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ParticipiantList() {
+    return (
+        <div>
+            <h1> This is a list of participants</h1>
+        </div>
+    )
+}
+
+export default ParticipiantList
