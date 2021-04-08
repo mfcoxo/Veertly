46cd2084659f77865a6b153b4dc6aec6ea4c68f3
Brittany Blake <brittany.blake49@gmail.com>
2021-04-08 14:02:09 +0200

    remove participant reducer

--- src/redux/reducers/RootReducer.js
+++ src/redux/reducers/RootReducer.js
@@ -1,10 +1,8 @@
 import { combineReducers } from "redux";
 import ParticipantListReducer from "./ParticipantListReducer";
-import ParticipantReducer from "./ParticipantReducer";
 
 const RootReducer = combineReducers({
   ParticipantList: ParticipantListReducer,
-  Participant: ParticipantReducer,
 });
 
 export default RootReducer;
